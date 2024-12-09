83abfda28973c7fe14d53978def7f86fb44f7a47
Psy Cisneros <psy@psysviewfinder.com>
2024-12-08 21:45:09 -0800

    Main class for collaboration.

---
+++ src/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * A menu where the user can interact with the social media system with admin rights.
+ * @author Psy Cisneros
+ * @author Aune Mitchell
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
